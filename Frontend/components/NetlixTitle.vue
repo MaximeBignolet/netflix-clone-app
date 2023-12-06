bfb968ded86b9a3df0e3d96dcf48ffa1263d07b5
Maxime Bignolet <maxime.bignolet@rhinos-groupe.com>
2023-12-06 19:55:25 +0100

    finish carousel + add login button and road with helpers

--- Frontend/components/NetlixTitle.vue
+++ Frontend/components/NetlixTitle.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="flex justify-center lg:block lg:mt-0">
-    <img src="https://fontmeme.com/permalink/231203/f3be64cfdd4aabfcccf33479f5a48e78.png" alt="logo netflix" class=" lg:text-6xl text-[#E50914] lg:font-extrabold lg:p-10 lg:absolute"/>
+    <div>
+      <img
+        src="https://fontmeme.com/permalink/231203/f3be64cfdd4aabfcccf33479f5a48e78.png"
+        alt="logo netflix"
+        class="lg:text-6xl text-[#E50914] lg:font-extrabold lg:p-10 lg:absolute"
+      />
+    </div>
   </div>
 </template>
--- Frontend/components/login/SignUp.vue
+++ Frontend/components/login/SignUp.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import SignupForm from "~/components/login/SignupForm.vue";
+import { RouteHelper } from "~/helpers/route-helper";
+
+const onClickNavigateToLoginPage = () => {
+  navigateTo(RouteHelper.LOGIN);
+};
 </script>
 
 <template>
-  <div class="z-20">
+  <div class="z-20 relative">
     <NetlixTitle />
+    <p
+      class="bg-[#E50914] text-white p-1.5 text-lg font-bold rounded-md w-1/12 absolute right-8 top-16 cursor-pointer text-center"
+      @click="onClickNavigateToLoginPage"
+    >
+      Login
+    </p>
   </div>
   <div class="relative">
-    <v-carousel height="" hide-delimiters show-arrows="hover">
+    <v-carousel height="980" hide-delimiters show-arrows="hover">
       <v-carousel-item src="/carousel-signup/signup_bg.png" cover class="z-0">
         <div class="h-full flex flex-col justify-center">
           <h2 class="text-white text-5xl w-1/3 pl-28 font-semibold">
@@ -21,7 +32,7 @@ import SignupForm from "~/components/login/SignupForm.vue";
         </div>
       </v-carousel-item>
       <v-carousel-item
-        src="/carousel-signup/christmas_series.png"
+        src="/carousel-signup/christmas_series.webp"
         cover
         class="z-0"
       >
@@ -49,11 +60,31 @@ import SignupForm from "~/components/login/SignupForm.vue";
           <SignupForm />
         </div>
       </v-carousel-item>
-      <v-carousel-item
-        src="/carousel-signup/the_crown.jpeg"
-        fill
-        class="z-0"
-      ></v-carousel-item>
+      <v-carousel-item src="/carousel-signup/the_crown.webp" cover class="z-0">
+        <div class="h-full flex flex-col justify-center">
+          <div>
+            <p
+              class="text-lg ml-28 bg-[#040F03]/50 rounded-lg w-fit p-2 font-semibold"
+            >
+              <font-awesome-icon icon="fire" class="text-[#FF8A15] mr-2" /><span
+                class="text-white"
+                >Tendances actuelles</span
+              >
+            </p>
+            <img
+              src="/carousel-signup/the_crown_paragraph.webp"
+              alt="Fête de Noël"
+              class="pl-28 h-14 py-3"
+            />
+            <p class="text-white text-base my-3 w-[38%] pl-28 mt-5">
+              Inspirée de faits réels, cette adaptation retrace l'histoire de la
+              reine Élisabeth II, évoquant les événements politiques et
+              personnels qui ont marqué son règne.
+            </p>
+          </div>
+          <SignupForm />
+        </div>
+      </v-carousel-item>
     </v-carousel>
   </div>
 </template>
